269b6190c1ce3305a42860e16d14b4131998f750
陳泰儒 <hello19881990@gmail.com>
2017-05-18 12:22:08 +0800

    地圖導引流程調整

--- Assets/script/p7/navigationCtrl.cs
+++ Assets/script/p7/navigationCtrl.cs
@@ -6,11 +6,32 @@ using UnityEngine.UI;
 public class navigationCtrl : MonoBehaviour 
 {
 	[SerializeField]
+	private Sprite[] spriteList;
+	[SerializeField]
+	private Sprite[] arrowSprite;
+	[SerializeField]
+	private float swapSec = 3.0f;
+
+	[SerializeField]
 	private page7Ctrl pageCtrl;
 	[SerializeField]
-	private GameObject floor;
-	void Start () {
-		
+	private Image floor;
+	[SerializeField]
+	private Image arrow;
+	[SerializeField]
+	private Vector3 arrowPos1 = Vector3.zero;
+	[SerializeField]
+	private Vector3 arrowPos2 = Vector3.zero;
+	[SerializeField]
+	private Vector3 arrowPos3 = Vector3.zero;
+
+	private List<Vector3> arrowPosList;
+	private int spriteIndex = 0;
+	void Awake () {
+		arrowPosList = new List<Vector3> ();
+		arrowPosList.Add (arrowPos1);
+		arrowPosList.Add (arrowPos2);
+		arrowPosList.Add (arrowPos3);
 	}
 	
 	// Update is called once per frame
@@ -20,15 +41,57 @@ public class navigationCtrl : MonoBehaviour
 
 	public void init()
 	{
-		playAni ani =  floor.GetComponent<playAni> ();
-		ani.stop ();
+		StopAllCoroutines ();
 
-		ani.setLoop (false, showHomeBtn );
-		ani.play ();
+		spriteIndex = 0;
+		StartCoroutine( setNavigation (spriteIndex) );
 	}
 
-	public void showHomeBtn()
+	public IEnumerator setNavigation( int index )
+	{
+		floor.sprite = spriteList [index];
+
+		if (index <= (arrowSprite.Length - 1)) 
+		{
+			arrow.gameObject.SetActive (true);
+			arrow.sprite = arrowSprite[index];
+			arrow.SetNativeSize ();
+			arrow.transform.localPosition = arrowPosList[index];
+
+			tweenAlpha tween = arrow.GetComponent<tweenAlpha> ();
+			tween.play ();
+		}
+		else 
+		{
+			arrow.gameObject.SetActive (false);
+		}
+
+		if (spriteIndex >= (spriteList.Length - 1)) 
+		{
+			showHomeBtn ();
+		}
+
+		yield return new WaitForSeconds (swapSec);
+		nextNavigation ();
+	}
+
+	private void showHomeBtn()
 	{
 		pageCtrl.showHomeBtn ();
+	}
+
+	private void nextNavigation()
+	{
+		StopAllCoroutines ();
+
+		if (spriteIndex >= (spriteList.Length - 1)) 
+		{
+			return;
+		}
+
+		if (!gameObject.activeInHierarchy) {return;}
+
+		spriteIndex++;
+		StartCoroutine (setNavigation( spriteIndex ));
 	}
 }
